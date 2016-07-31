b2629b474e6e2508e6881533439b3667b459d4fa
stSoftware <nigel@stsoftware.com.au>
2016-07-31 11:55:49 +1000

    Create sync.sh

---
+++ sync.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+send_files(){
+  SERVER=$1
+  echo "send files to $SERVER"
+  rsync -rtpqu certs $SERVER:
+  rsync -rtpqu challenges $SERVER:
+  rsync -rtpqu csr $SERVER:
+  rsync -rtpqu sites $SERVER:
+  rsync -rtpqu keys $SERVER:
+}
+
+#send_files www2
