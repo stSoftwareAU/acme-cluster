7e139a0a5d4362060af4ddf5ef0d76d918d6665a
stSoftware <nigel@stsoftware.com.au>
2016-10-03 16:04:54 +1100

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -3,11 +3,11 @@
 send_files(){
   SERVER=$1
   echo "send files to $SERVER"
-  rsync -rtpqu certs $SERVER:
-  rsync -rtpqu challenges $SERVER:
-  rsync -rtpqu csr $SERVER:
-  rsync -rtpqu sites $SERVER:
-  rsync -rtpqu keys $SERVER:
+  rsync -rtpqu ~/certs $SERVER:
+  rsync -rtpqu ~/challenges $SERVER:
+  rsync -rtpqu ~/csr $SERVER:
+  rsync -rtpqu ~/sites $SERVER:
+  rsync -rtpqu ~/keys $SERVER:
 }
 
 #send_files www2
